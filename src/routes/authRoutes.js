32954a89af2262e4c3191fb0bb51ec4cd8ab46c7
Alexandra Khalimon <lexikhalimon@gmail.com>
2025-10-19 16:51:44 +0300

    task-refresh

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,12 +4,13 @@ import {
   loginUserSchema,
   registerUserSchema,
 } from '../validation/authValidation.js';
-import { loginUser, logoutUser, registerUser } from '../controllers/authController.js';
+import { loginUser, logoutUser, registerUser, refreshSession } from '../controllers/authController.js';
 
 const router = Router();
 
 router.post('/auth/register', celebrate(registerUserSchema), registerUser);
 router.post('/auth/login', celebrate(loginUserSchema), loginUser);
 router.post('/auth/logout', logoutUser);
+router.post('/auth/refresh', refreshSession);
 
 export default router;
