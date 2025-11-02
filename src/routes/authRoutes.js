b1a4154755fca22d1b2ea647bb23a802fee5849d
Mr.Igor <iv_mirsoft@ukr.net>
2025-11-02 14:38:52 +0200

    reset-pwd-mail

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,13 +3,16 @@ import { Router } from 'express';
 import {
   loginUserSchema,
   registerUserSchema,
+  resetMailSchema,
 } from '../validation/authValidation.js';
-import { loginUser, logoutUser, registerUser } from '../controllers/authController.js';
+import { loginUser, logoutUser, registerUser, resetMail } from '../controllers/authController.js';
 
 const router = Router();
 
 router.post('/auth/register', celebrate(registerUserSchema), registerUser);
 router.post('/auth/login', celebrate(loginUserSchema), loginUser);
 router.post('/auth/logout', logoutUser);
+
+router.post('/auth/request-reset-email', celebrate(resetMailSchema), resetMail);
 
 export default router;
